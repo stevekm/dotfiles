e9da35c8d6c366710626c8f02eab2029eed1870a
Stephen Kelly <stevekm@users.noreply.github.com>
2020-04-16 10:40:41 -0400

    refactor console colors

---
+++ console.colors.sh
@@ -0,0 +1,16 @@
+##############################################################################
+# Console Colors for Local Computer
+# https://unix.stackexchange.com/questions/124407/what-color-codes-can-i-use-in-my-ps1-prompt
+##############################################################################
+
+case "${HOSTNAME}" in
+    "quark") # light blue
+        export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[94m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+        ;;
+    silo|juno|j*) # green for known remote servers
+        export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[92m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+        ;;
+    *) # red for everything else
+        export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[31m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+        ;;
+    esac
