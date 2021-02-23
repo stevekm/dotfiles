ed66292c1782ffec48eb3922d4f4ef048c2c9c9f
Stephen Kelly <stevekm@users.noreply.github.com>
2021-02-23 02:12:54 +0000

    add EC2 colors

--- console.colors.sh
+++ console.colors.sh
@@ -1,6 +1,7 @@
 ##############################################################################
 # Console Colors for Local Computer
 # https://unix.stackexchange.com/questions/124407/what-color-codes-can-i-use-in-my-ps1-prompt
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
 ##############################################################################
 
 case "${HOSTNAME}" in
@@ -12,6 +13,9 @@ case "${HOSTNAME}" in
         ;;
     silo|juno|j*) # green for known remote servers
         export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[92m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+        ;;
+    *.ec2.*) # orange for AWS EC2
+        export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[\e[38;5;214m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
         ;;
     *) # red for everything else
         export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[31m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
