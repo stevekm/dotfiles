ca740cbfd488cc4d2325084363aceb2ad9dfa9c4
Stephen Kelly <stevekm@users.noreply.github.com>
2022-05-14 16:53:25 -0400

    update zsh prompt

---
+++ console.zsh
@@ -0,0 +1,13 @@
+# zsh console customizations
+# default: /etc/zshrc
+
+# history
+HISTSIZE=2000000
+SAVEHIST=1000000
+
+
+# prompt
+# Default prompt
+# PS1="%n@%m %1~ %# "
+PS1="%F{cyan}%n%f@%F{red}%m%f:%F{yellow}%~%f$ "
+precmd() { printf '\n\033[0;35m[%s]\033[m\n' "$(date +%F\ %T)"; }
