934982beba5db20d0af662d7c9ebf865cbb5c9fb
Stephen Kelly <stevekm@users.noreply.github.com>
2022-05-20 14:16:03 -0400

    fix PS1 for zsh

--- console.zsh
+++ console.zsh
@@ -10,5 +10,10 @@ SAVEHIST=1000000
 # prompt
 # Default prompt
 # PS1="%n@%m %1~ %# "
-PS1="%F{cyan}%n%f@%F{red}%m%f:%F{yellow}%~%f$ "
+# https://zsh-prompt-generator.site/
+# https://apple.stackexchange.com/questions/30552/os-x-computer-name-not-matching-what-shows-on-terminal
+_UNAME="$(uname)"
+_COMPUTER_NAME="%m"
+if [[ "${_UNAME}" == "Darwin" ]]; then _COMPUTER_NAME="$(scutil --get ComputerName)" ; fi
+PS1="%F{cyan}%n%f@%F{red}${_COMPUTER_NAME}%f:%F{yellow}%~%f$ "
 precmd() { printf '\n\033[0;35m[%s]\033[m\n' "$(date +%F\ %T)"; }
