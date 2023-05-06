f1773eb7cc6628afb615b5f78b97f8b2cd0b4792
Stephen Kelly <stevekm@users.noreply.github.com>
2023-05-06 13:38:37 -0400

    add cli arg to nvidia-pl script

--- nvidia-pl.sh
+++ nvidia-pl.sh
@@ -5,20 +5,21 @@
 # set the script to run at boot
 # https://askubuntu.com/questions/290099/how-to-run-a-script-during-boot-as-root/290107#290107
 # put this in crontab on boot;
-# @reboot sh /home/(username)/nvidia-pl.sh 
+# @reboot sh /home/(username)/nvidia-pl.sh
 
-# you need to add the following lines to the sudoers file to allow the nvidia-smi command to be run as sudo without the need for a password. 
-# https://www.howtogeek.com/842739/how-to-add-a-user-to-the-sudoers-file-in-linux/ 
+# you need to add the following lines to the sudoers file to allow the nvidia-smi command to be run as sudo without the need for a password.
+# https://www.howtogeek.com/842739/how-to-add-a-user-to-the-sudoers-file-in-linux/
 # sudo visudo
 # (your username) ALL=(ALL) NOPASSWD: /usr/bin/nvidia-persistenced
 # (your username) ALL=(ALL) NOPASSWD: /usr/bin/nvidia-smi
 
+PL=${1:-240}
 
 #Enable persistence mode
 sudo nvidia-smi -pm ENABLED
 
 #Set power limit to 240Watts
-sudo nvidia-smi -pl 240
+sudo nvidia-smi -pl "$PL"
 
 # check current power settings with
 # sudo nvidia-smi -q -d POWER
