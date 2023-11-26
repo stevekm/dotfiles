743ceee4e897aa3831179d9dd184f43da4acb94d
Stephen Kelly <stevekm@users.noreply.github.com>
2023-11-26 12:34:49 -0500

    update colors for nuc

--- console.colors.sh
+++ console.colors.sh
@@ -7,7 +7,7 @@
 # NOTE: default is export PS1='\u@\h \W\\$'
 
 case "${HOSTNAME}" in
-    "quark") # light blue
+    "quark"|*nuc*) # light blue
         export PS1="\[\033[96m\]\u\[\033[m\]@\[\033[94m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
         ;;
     hg*) # gray
